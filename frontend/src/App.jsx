ff1ab42b1965a8d71e3ce2cfd540e0e35df22216
Shubham kahar <Shubhamkahar196@gmail.com>
2025-05-19 00:14:26 +0530

    Completing the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Doctor from './pages/Doctor'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/doctors" element={<Doctor/>}/>
+        <Route path="/doctors/speciality" element={<Doctor/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/my-profile" element={<MyProfile/>}/>
+        <Route path="/my-appointment" element={<MyAppointments/>}/>
+        <Route path="/appointment/:docId" element={<Appointment/>}/>
+      </Routes>
+    </div>
   )
 }
 
